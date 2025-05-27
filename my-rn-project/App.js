f3fed59022716ca2ff032c14a0aa5d9f3f52c586
Geronimo Tiscornia <gero.tisco@gmail.com>
2025-05-27 18:18:21 -0300

    probando git

--- my-rn-project/App.js
+++ my-rn-project/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+ 
 import Register from './src/screens/Register/Register';
 import Login from './src/screens/Login/Login';
 import Home from './src/screens/Home/Home';
