6b123fc197d497ae128bd749959feb6d95722a63
pedroguyot04 <pedroguyot99@gmail.com>
2025-05-27 18:26:32 -0300

    Modifique el archivo de navegacion

--- my-rn-project/App.js
+++ my-rn-project/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import StackNavigation from './src/navigation/StackNavigation';
 import Register from './src/screens/Register/Register';
 import Login from './src/screens/Login/Login';
 import Home from './src/screens/Home/Home';
@@ -12,11 +12,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Register' component={Register} options={{ headerShown: false }} />
-        <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} />
-      </Stack.Navigator>
+      <StackNavigation />
     </NavigationContainer>
 
   );
---
+++ my-rn-project/src/navigation/StackNavigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Register from '../screens/Register/Register'
+import Login from "../screens/Login/Login";
+import Home from "../screens/Home/Home";
+
+const Stack = createNativeStackNavigator();
+
+function StackNavigation() { 
+    return(
+        <Stack.Navigator>
+        <Stack.Screen 
+            name='Register' 
+            component={Register} 
+            options={{ headerShown: false }} />
+        <Stack.Screen 
+            name='Login' 
+            component={Login} 
+            options={{ headerShown: false }} />
+        <Stack.Screen 
+            name='Home' 
+            component={Home} 
+            options={{ headerShown: false }} />
+      </Stack.Navigator>
+    )
+}
+
+export default StackNavigation
