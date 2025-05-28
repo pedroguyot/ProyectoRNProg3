9f1eec87a8fcd9478afa81e12e58934680423767
Geronimo Tiscornia <gero.tisco@gmail.com>
2025-05-28 19:22:54 -0300

    casi terminado el login

--- my-rn-project/App.js
+++ my-rn-project/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
- import StackNavigation from './src/navigation/StackNavigation';
+import StackNavigation from './src/navigation/StackNavigation';
 import Register from './src/screens/Register/Register';
 import Login from './src/screens/Login/Login';
 import Home from './src/screens/Home/Home';
--- my-rn-project/src/screens/Login/Login.js
+++ my-rn-project/src/screens/Login/Login.js
@@ -1,23 +1,80 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { auth } from '../../components/firebase/config';
+import { TextInput } from 'react-native-web';
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            activo: false,
-        }
+            email: '',
+            password: '',
+            error: ''
+        };
     }
 
+    login(email, password) {
+        auth.signInWithEmailAndPassword(email, password)
+            .then(response => {
+                this.props.navigation.navigate('Home')
+            })
+            .catch(error => {
+                this.setState({ error: 'Credenciales Invalidas. Porfavor Reintente.' });
+            })
+    }
 
+    
     render() {
         return (
-            <View>
-                <Text>Pantalla Login</Text>
+            <View style={styles.conteiner}>
+                <Text style={styles.titulo}>Iniciar Sesion</Text>
+
+                <TextInput
+                    placeholder='Email'
+                    keyboardType='emial-address'
+                    onChangeText={(text) => this.setState({ email: text })}
+                    value={this.setState.email}
+                />
+
+                <TextInput
+                    placeholder='Contraseña'
+                    keyboardType={true}
+                    onChangeText={(text) => this.setState({ password: text })}
+                    value={this.setState.password}
+                />
+
+                {
+                    this.state.error !== '' ? (
+                        <Text style={styles.error}>{this.state.error}</Text>
+                    ) : null
+                }
+
+                <TouchableOpacity
+                    onPress={() => this.login(this.state.email, this.state.password)}>
+
+                    <Text style={styles.botonText}>Ingresar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('Register')}>
+                    <Text>¿No tenes cuenta? Regitrate aca</Text>
+                </TouchableOpacity>
+
             </View>
         )
     }
 }
 
-
 export default Login;
+
+const styles = StyleSheet.create({
+    conteiner: {
+        flex: 1,
+        alignItems: center
+    },
+    titulo: {
+        fontWeight: 'bold',
+    },
+    botonText: {
+        fontWeight: 'bold',
+    }
+})
