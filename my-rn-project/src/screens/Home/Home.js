5b7324e286a62be09adab324f9de6a181dd13066
Geronimo Tiscornia <gero.tisco@gmail.com>
2025-05-30 19:06:41 -0300

    terminando el home

--- my-rn-project/src/screens/Home/Home.js
+++ my-rn-project/src/screens/Home/Home.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { db } from '../../firebase/config';
+import Posteo from '../Posteo/Posteo';
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            activo: false,
-        }
+            posts: [],
+            loading: true
+        };
     }
 
+    componentDidMount() {
+        db.collection('posts')
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(docs => {
+                let posteos = [];
+
+                docs.forEach(doc => {
+                    posteos.push({
+                        id: doc.id,
+                        data: doc.data()
+                    });
+                });
+
+                this.setState({
+                    posts: posteos,
+                    loading: false
+                });
+            });
+    }
 
     render() {
         return (
-            <View>
-                <Text>Pantalla Home</Text>
+            <View style={styles.container}>
+                {this.state.loading ? (
+                    <ActivityIndicator size="large" color="blue" />
+                ) : (
+
+                    <FlatList
+                        data={this.state.posts}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => <Posteo postData={item} />}
+                    />
+                )}
             </View>
         )
     }
 }
 
-
 export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 10
+    }
+});
